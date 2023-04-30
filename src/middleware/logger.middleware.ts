a04a7bd111bcf1e75037f3381286cec0db7ceb97
Ayush Shashank <ayush.bibhas@gmail.com>
2023-04-30 16:45:55 +0530

    modified logging

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,13 +1,18 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
-    console.log(`${req.method}: ${req.path}`);
-    res.on('finish', () =>
-      console.log(` => ${res.statusCode}: ${res.statusMessage}`),
-    );
+    Logger.log(req.path, req.method);
+    res.on('finish', () => {
+      if (res.statusCode === 200 || res.statusCode === 201)
+        Logger.log(res.statusMessage, res.statusCode.toString());
+      else if (res.statusCode === 204 || res.statusCode === 304)
+        Logger.warn(res.statusMessage, res.statusCode.toString());
+      else Logger.error(res.statusMessage, res.statusCode.toString());
+    });
     next();
   }
 }
